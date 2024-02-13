48d12d8e35ea2060653b43bb256390de270d9e47
oivalian <oivaliann@gmail.com>
2024-02-13 23:36:10 +1100

    Added reverse args | minor refactor

--- tkgrider/tkgrider.py
+++ tkgrider/tkgrider.py
@@ -2,38 +2,48 @@
 class Pack:
 
     @classmethod
-    def packcol(cls, widget_list, max_col, y=None, x=None, iy=None, ix=None, s=""):
+    def packcol(cls, widget_list, max_col, y=None, x=None, iy=None, ix=None, stick=None, reverse=False):
         row_pos = 0
-        col_pos = 0
+        col_pos = max_col if reverse else 0
         for widget in widget_list:
-            widget.grid(row=row_pos, column=col_pos, pady=y, padx=x, ipadxy=iy, ipadx=ix, sticky=s)
-            col_pos += 1
-            if col_pos == max_col:
-                row_pos += 1
-                col_pos = 0
-
-    @classmethod
-    def packrow(cls, widget_list, max_row, y=None, x=None, iy=None, ix=None, s=""):
-        row_pos = 0
-        col_pos = 0
-        for widget in widget_list:
-            widget.grid(row=row_pos, column=col_pos, pady=y, padx=x, ipadxy=iy, ipadx=ix, sticky=s)
-            row_pos += 1
-            if row_pos == max_row:
+            widget.grid(row=row_pos, column=col_pos, pady=y, padx=x, ipady=iy, ipadx=ix, sticky=stick)
+            if not reverse:
                 col_pos += 1
-                row_pos = 0
+                if col_pos == max_col:
+                    row_pos += 1
+                    col_pos = 0
+            elif reverse:
+                col_pos -= 1
+                if col_pos == 0:
+                    row_pos += 1
+                    col_pos = max_col
 
     @classmethod
-    def pack(cls, widget_list, max_row, max_col, y=None, x=None, iy=None, ix=None, s=""):
+    def packrow(cls, widget_list, max_row, y=None, x=None, iy=None, ix=None, s=None, reverse=False):
+        col_pos = 0
+        row_pos = max_row if reverse else 0
+        for widget in widget_list:
+            widget.grid(row=row_pos, column=col_pos, pady=y, padx=x, ipady=iy, ipadx=ix, sticky=s)
+            if not reverse:
+                row_pos += 1
+                if row_pos == max_row:
+                    col_pos += 1
+                    row_pos = 0
+            elif reverse:
+                row_pos -= 1
+                if row_pos == 0:
+                    col_pos += 1
+                    row_pos = max_row
+
+    @classmethod
+    def pack(cls, widget_list, max_row, max_col, y=None, x=None, iy=None, ix=None, s=None):
         row_pos = 0
         col_pos = 0
         for widget in widget_list:
-            widget.grid(row=row_pos, column=col_pos, pady=y, padx=x, ipadxy=iy, ipadx=ix, sticky=s)
+            widget.grid(row=row_pos, column=col_pos, pady=y, padx=x, ipady=iy, ipadx=ix, sticky=s)
             col_pos += 1
             if col_pos == max_col:
                 col_pos = 0
                 row_pos += 1
                 if row_pos == max_row:
                     break
-
-
